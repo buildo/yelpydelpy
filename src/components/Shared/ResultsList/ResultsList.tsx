d4b95addcf0653a6a687e0e26441aae36e9a959d
aSlug <marpi93@icloud.com>
2020-01-20 16:08:16 +0100

    use io-ts to define YelpSearchResponse

---
+++ src/components/Shared/ResultsList/ResultsList.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import View from '../../Basic/View';
+
+type Props = {};
+
+export default class ResultsList extends React.Component<Props, {}> {
+  render() {
+    return <View className="results-list" grow></View>;
+  }
+}

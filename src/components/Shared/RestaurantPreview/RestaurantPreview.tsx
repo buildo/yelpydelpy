45cbcc0fe453bf245faf0ad2a81f49997924b215
aSlug <marpi93@icloud.com>
2020-01-23 13:14:28 +0100

    add placeholder if photo is missing

--- src/components/Shared/RestaurantPreview/RestaurantPreview.tsx
+++ src/components/Shared/RestaurantPreview/RestaurantPreview.tsx
@@ -4,6 +4,7 @@ import View from '../../Basic/View';
 
 import './restaurantPreview.scss';
 
+const noPhotoPlaceholder = require('./../../../images/no-photo.png');
 const star = require('./../../../images/star.png');
 
 type Props = {
@@ -14,6 +15,8 @@ export default class RestaurantPreview extends React.Component<Props, {}> {
   render() {
     const rest = this.props.restaurant;
 
+    const photo = rest.image_url && rest.image_url !== '' ? rest.image_url : noPhotoPlaceholder;
+
     return (
       <View className="restaurant-preview" grow>
         <View
@@ -22,7 +25,7 @@ export default class RestaurantPreview extends React.Component<Props, {}> {
           vAlignContent="center"
           style={{ height: '120px', width: '120px' }}
         >
-          <img style={{ width: '90%', height: '90%' }} src={rest.image_url} alt="image" />
+          <img style={{ width: '90%', height: '90%' }} src={photo} alt={'image'} />
         </View>
 
         <View className="info-frame" grow column>
